0f2c39f599f804755fe3c03066dbe6fd45d6dfda
aditi03521 <heyaditihere@gmail.com>
2025-09-17 01:12:03 +0530

    Add files via upload

---
+++ findmissingno.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class findmissingno{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("ENTER SIZE");
+int n=sc.nextInt();
+int[] a=new int[n];
+System.out.println(n);
+for (int i=0;i<n;i++)
+{   System.out.println("enter no to be inserted in array");
+    a[i]=sc.nextInt();
+}
+int actsum=(n*(n+1))/2;
+int sum = 0;
+for (int i=0;i<n;i++)
+{   sum=sum+a[i];
+
+}
+int missno = actsum - sum ;
+System.out.println("missing number:"+ missno);
+
+}
+}
