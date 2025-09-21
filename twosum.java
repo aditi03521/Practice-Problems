4bdb3218204b1f3e5092fe838b562a5e5c4ca193
aditi03521 <heyaditihere@gmail.com>
2025-09-21 19:58:42 +0530

    Add files via upload

---
+++ twosum.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class twosum{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("ENTER SIZE");
+int n=sc.nextInt();
+int[] a=new int[n];
+System.out.println(n);
+for (int i=0;i<n;i++)
+{   System.out.println("enter no to be inserted in array");
+    a[i]=sc.nextInt();
+}
+ System.out.println("enter output sum");
+ int osum= sc.nextInt();
+ for ( int i=0;i<n;i++)
+ {
+    for(int j=0;j<n;j++){
+        if(a[i]+a[j]==osum){
+            if(a[j]==a[i]){
+                break;
+            }
+            else{
+                 System.out.println(i+","+j);
+            }
+        }
+    }
+ }
+}
+}
